a2ed86249af548cdcd24920a2deebc2bb941633c
Prasoon-Verma-21 <prasoon21v@gmail.com>
2026-01-23 00:48:01 +0530

    updated the API Key

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBpSJ6atHMQGJyERizTXFdEZhavZoR6tqI",
-    authDomain: "new-education-era-2026.firebaseapp.com",
-    projectId: "new-education-era-2026",
-    storageBucket: "new-education-era-2026.firebasestorage.app",
-    messagingSenderId: "82348306463",
-    appId: "1:82348306463:web:9b6f47dc6b6d984e24652a",
-    measurementId: "G-3QG7KGZ5M1"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase services
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app); // For Login/Signup
-export const db = getFirestore(app); // For storing User Roles
+export const auth = getAuth(app);
+export const db = getFirestore(app);
